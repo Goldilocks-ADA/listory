80d4e27d23170379b575d059caf28902d3296847
Jogi Oktavianus <34507947+JogiOktavianus@users.noreply.github.com>
2020-11-12 01:00:01 +0700

    Initial Commit

--- listory/ListoryAlbumController.swift
+++ listory/ListoryAlbumController.swift
@@ -15,7 +15,7 @@ struct CustomData {
     var url = String()
 }
 
-class ListoryAlbumController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+class ListoryAlbumController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     var recordings = [URL]()
     var player: AVAudioPlayer!
@@ -71,7 +71,7 @@ class ListoryAlbumController: UIViewController, UIImagePickerControllerDelegate 
         imagePickerController.delegate = self
         
         //Alert Notification
-        let actionSheet = UIAlertController(title: "Listory would like to Access the Camera", message: "So you can take a picture from family albums", preferredStyle: .actionSheet)
+        let actionSheet = UIAlertController(title: "Add Image", message: "Would you like to take a picture from:", preferredStyle: .actionSheet)
         
         //Photo From Camera
         actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
@@ -94,32 +94,7 @@ class ListoryAlbumController: UIViewController, UIImagePickerControllerDelegate 
         //Cancel Button
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
 //        self.present(actionSheet, animated: true, completion: nil)
-        
-        //TabBarController
-        let tabBarVC = UITabBarController()
-        tabBarVC.tabBar.tintColor = .black
-        tabBarVC.tabBar.barTintColor = .none
-        tabBarVC.view.backgroundColor = .clear
-        tabBarVC.tabBar.barStyle = .default
-
-//        let tb1 = UINavigationController(rootViewController: FirstListoryAlbumController())
-//        let tb2 = UINavigationController(rootViewController: SecondListoryAlbumController())
-//        let tb3 = UINavigationController(rootViewController: ThirdListoryAlbumController())
-        
-        let tb1 = FirstViewController()
-        let tb2 = SecondViewController()
-        let tb3 = ThirdViewController()
-
-        tb1.tabBarItem.image = UIImage(named: "btnPhoto")
-        tb2.tabBarItem.image = UIImage(named: "btnAudio")
-        tb3.tabBarItem.image = UIImage(named: "btnForYou")
-        
-        tabBarVC.setViewControllers([tb1,tb2, tb3], animated: false)
-        
-        
-        tabBarVC.modalPresentationStyle = .fullScreen
-        present(tabBarVC, animated: false, completion: nil)
-        
+            
         //Popover Position
         if let popoverController = actionSheet.popoverPresentationController {
             popoverController.sourceView = self.view
@@ -242,31 +217,5 @@ extension UINavigationBar {
     self.setBackgroundImage(UIImage(), for: .default)
     self.shadowImage = UIImage()
     self.isTranslucent = true
-    }
-}
-
-class TabBarController: UIViewController {
-    override func viewDidLoad() {
-        super .viewDidLoad()
-        self.view.backgroundColor = .clear
-    }
-}
-
-class FirstViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
-    }
-}
-
-class SecondViewController: UIViewController {
-    override func viewDidLoad() {
-        view.backgroundColor = .yellow
-    }
-}
-
-class ThirdViewController: UIViewController {
-    override func viewDidLoad() {
-        view.backgroundColor = .blue
     }
 }
