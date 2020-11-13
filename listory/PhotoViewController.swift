ebce50b1916035a588a4680e77cd8febc04e1c89
Jogi Oktavianus <34507947+JogiOktavianus@users.noreply.github.com>
2020-11-13 12:22:00 +0700

    initial commit

--- listory/PhotoViewController.swift
+++ listory/PhotoViewController.swift
@@ -11,7 +11,8 @@ class PhotoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .clear
+        
         
     }
 
